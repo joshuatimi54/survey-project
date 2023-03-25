f125c0c778fed0262ebe96e7273d03f0ee6fbc5e
joshuatimi54 <105829140+joshuatimi54@users.noreply.github.com>
2023-03-25 03:06:36 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 const User = require('../models/user');
 
-// quest_testpage
-const quest_testpage_get = (_req, res) => {
+// user_testpage
+const user_testpage_get = (_req, res) => {
     res.render('quests/testpage', { title: 'Answer the questions' });
 }
 
-// quest_testpage
-const quest_testpage = (req, res) => {
+// user_testpage
+const user_testpage = (req, res) => {
     const user = new User(req.body);
 
     user.save()
@@ -18,8 +18,52 @@ const quest_testpage = (req, res) => {
     })
 }
 
-// module export to questRoutes
+// user_testedusers
+const user_testedusers_get = (_req, res) => {
+   User.find().count({ createdAt: -1 })
+    .then((result) => {
+        res.render('quests/testedusers', { title: 'Result for survey', users: result  });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+// user_usersurvey
+const user_usersurvey_get = (req, res) => {
+    const id = req.params.id;
+
+   User.findById(id)
+    .then(result => {
+        res.render('quests/usersurvey', { title: 'Result for individual survey', users: result  });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+
+// user_create_delete
+const user_delete = (req, res) => {
+    const id = req.params.id;
+
+    User.findByIdAndDelete(id)
+    .then(_result => {
+        res.json({ redirect: '/quests/testedusers' });
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
+
+ 
+// module export to userRoutes
 module.exports = {
-    quest_testpage_get,
-    quest_testpage
+    user_usersurvey_get,
+    user_testedusers_get,
+    user_testpage_get,
+    user_delete,
+    user_testpage
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const questController = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const userRouter = express.Router();
 
-userRouter.post('/', questController.quest_testpage);
-userRouter.get('/testpage', questController.quest_testpage_get);
+userRouter.post('/', userController.user_testpage);
+userRouter.get('/testpage', userController.user_testpage_get);
+userRouter.get('/testedusers', userController.user_testedusers_get);
+userRouter.get('/:id', userController.user_usersurvey_get);
+userRouter.delete('/:id', userController.user_delete);
 
 
 
